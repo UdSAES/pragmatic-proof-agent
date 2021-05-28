db958bdf579dd74dfd883aeec8fb429eddab3991
Moritz Stüber <moritz.stueber@aut.uni-saarland.de>
2021-05-28 12:11:46 +0200

    Set up initial structure/libraries for implementation of agent

---
+++ agent.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+"""Software agent for hypermedia API composition and execution."""
+
+from invoke import task
+from loguru import logger
+
+
+@task(help={"origin": "The root URL to the service instance"})
+def discover_restdesc(ctx, origin):
+    """Discover RESTdesc descriptions of service instance."""
+
+
+def eye_generate_proof(inputs):
+    """Generate proof using containerized EYE reasoner."""
+
+
+def solve_api_composition_problem(H, g, R, B):
+    """Solve API composition problem."""
+
+
+def solve_task():
+    """Collect definition of API composition problem."""
