b5f781144ee730943f9d39af63d4937cde84ead0
Moritz Stüber <moritz.stueber@aut.uni-saarland.de>
2021-05-29 11:43:28 +0200

    Implement proof generation using containerized EYE

--- agent.py
+++ agent.py
@@ -62,8 +62,43 @@ def discover_restdesc(ctx, origin):
     return descriptions
 
 
-def eye_generate_proof(inputs):
+# http://docs.pyinvoke.org/en/stable/concepts/invoking-tasks.html#iterable-flag-values
+@task(
+    iterable=["input_files"],
+    help={
+        "input_files": "The filenames of all input files",
+        "agent_goal": "The name of the .n3-file specifying the agent's goal",
+    },
+)
+def eye_generate_proof(ctx, input_files, agent_goal):
     """Generate proof using containerized EYE reasoner."""
+
+    logger.info("Generating proof using EYE...")
+
+    dir_n3 = os.getenv("AGENT_TMP")
+    workdir = "/mnt"
+    image_name = os.getenv("EYE_IMAGE_NAME")
+    prefix = (
+        "docker run "
+        "-i "
+        "--rm "
+        "--name eye "
+        f"-v {dir_n3}:{workdir} "
+        f"-w {workdir} "
+        f"{image_name} "
+    )
+
+    options = "--quiet --tactic limited-answer 1"
+    filenames = " ".join(input_files)
+    cmd_container = f"{options} {filenames} --query {agent_goal}"
+
+    cmd = prefix + cmd_container
+    logger.debug(cmd)
+
+    result = ctx.run(cmd, hide=True)
+
+    logger.debug(f"Reasoning logs:\n{result.stderr}")
+    logger.debug(f"Proof deduced by EYE:\n{result.stdout}")
 
 
 def solve_api_composition_problem(H, g, R, B):
