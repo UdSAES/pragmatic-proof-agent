db6113a51f665db366adbd928caa6117e149f851
Moritz Stüber <moritz.stueber@aut.uni-saarland.de>
2021-06-04 12:47:22 +0200

    Specify expected MIME type for response explicitly

--- tests/test_server.py
+++ tests/test_server.py
@@ -19,16 +19,17 @@ class TestImageResizeAPI(object):
     origin = os.environ["IMG_API_ORIGIN"]
 
     @pytest.mark.parametrize(
-        "method, origin, path, accept, body, status_code",
+        "method, origin, path, accept, content_type, body, status_code",
         [
-            ("OPTIONS", origin, "/images", "text/n3", None, 200),
-            ("OPTIONS", origin, "/images/0", "text/n3", None, 200),
-            ("OPTIONS", origin, "/images/0/thumbnail", "text/n3", None, 200),
+            ("OPTIONS", origin, "/images", "text/n3", "text/n3", None, 200),
+            ("OPTIONS", origin, "/images/0", "text/n3", "text/n3", None, 200),
+            ("OPTIONS", origin, "/images/0/thumbnail", "text/n3", "text/n3", None, 200),
             (
                 "POST",
                 origin,
                 "/images",
                 "image/png",
+                "text/n3",
                 f"{test_data_base_path}/example.png",
                 201,
             ),
@@ -37,6 +38,7 @@ class TestImageResizeAPI(object):
                 origin,
                 "/images",
                 "text/n3",
+                "application/problem+json",
                 f"{test_data_base_path}/example.png",
                 406,
             ),
@@ -44,6 +46,7 @@ class TestImageResizeAPI(object):
                 "GET",
                 origin,
                 "/images/90007eb1c2af27c8fbac3fc6db2f801a",
+                "image/png",
                 "image/png",
                 f"{test_data_base_path}/example.png",
                 200,
@@ -53,29 +56,36 @@ class TestImageResizeAPI(object):
                 origin,
                 "/images/90007eb1c2af27c8fbac3fc6db2f801a/thumbnail",
                 "image/png",
+                "image/png",
                 None,
                 200,
             ),
-            ("GET", origin, "/images/_", "image/png", None, 404),
+            (
+                "GET",
+                origin,
+                "/images/_",
+                "image/png",
+                "application/problem+json",
+                None,
+                404,
+            ),
             (
                 "GET",
                 origin,
                 "/images/_/thumbnail",
                 "image/png",
+                "application/problem+json",
                 None,
                 404,
             ),
         ],
     )
-    def test_send_request(self, method, origin, path, accept, body, status_code):
+    def test_send_request(
+        self, method, origin, path, accept, content_type, body, status_code
+    ):
         href = f"{origin}{path}"
         headers = {"accept": accept}
         files = None
-
-        if status_code >= 400:
-            expected_content_type = "application/problem+json"
-        else:
-            expected_content_type = accept
 
         if body is not None:
             file_name = body.split("/")[-1]
@@ -86,4 +96,4 @@ class TestImageResizeAPI(object):
         r = requests.request(method, href, headers=headers, files=files)
 
         assert r.status_code == status_code
-        assert r.headers["content-type"].split(";")[0] == expected_content_type
+        assert r.headers["content-type"].split(";")[0] == content_type
