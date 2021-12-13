e9d434914f36934f9a725f1dc3544d0b5b229be5
Moritz Stüber <moritz.stueber@aut.uni-saarland.de>
2021-12-13 19:11:16 +0100

    Get rid of duplicate code for example definition

--- agent/__main__.py
+++ agent/__main__.py
@@ -144,77 +144,12 @@ def download_restdesc(ctx, origin, selector, directory, clean_tmp=False):
         "clean_tmp": "Set iff all files in $AGENT_TMP shall be deleted first",
     }
 )
-def get_thumbnail(ctx, origin, directory, clean_tmp=False):
-    """
-    Collect definition of specific API composition problem; then solve it.
-
-    This function is specific to the image-resizing example as it should be. The rest of
-    the code should be independent of any specific example, i.e. implement a generic
-    (maybe even somewhat intelligent) agent!
-    """
-
-    # Environment to be used when rendering templates using Jinja2
-    ENV = Environment(
-        loader=FileSystemLoader("examples/image_resizing"),
-        trim_blocks=True,
-        lstrip_blocks=True,
-    )
-
-    if clean_tmp == True:
-        delete_all_files(directory)
-
-    # Specify _initial state H_
-    initial_state = ENV.get_template("initial_state.n3.jinja")
-    H = "agent_knowledge.n3"
-
-    # Define the _goal state g_, i.e. the agent's objective
-    goal_state = ENV.get_template("agent_goal.n3.jinja")
-    g = "agent_goal.n3"
-
-    # Store initial state and the agent's goal as .n3 files on disk
-    # image_rdfterm = rdflib.Literal("example.png")  # XXX is hardcoding a good idea? No!
-    image_rdfterm = rdflib.URIRef(
-        "https://ontologies.msaas.me/example.ttl#example.png"
-    )  # XXX is hardcoding a good idea? No!
-    for template, filename in [(initial_state, H), (goal_state, g)]:
-        path = os.path.join(directory, filename)
-        with open(path, "w") as fp:
-            fp.write(template.render(image=image_rdfterm.n3()))
-
-    # Discover _description formulas R_ (RESTdesc descriptions)
-    language = os.getenv("IMG_API_LANG", "en")
-    logger.debug(f"Selected language '{language}'")
-
-    R = download_restdesc(ctx, origin, language, directory, False)
-
-    # Specify additional _background knowledge B_ [if applicable]
-    B = None
-
-    # Solve API composition problem
-    status = solve_api_composition_problem(ctx, directory, [H], g, R, B)
-
-    # Properly set exit code
-    if status == SUCCESS:
-        logger.info("Done! 😎")
-    else:
-        logger.error("💥 Terminating with non-zero exit code... ")
-
-    sys.exit(status)
-
-
-@task(
-    help={
-        "origin": "The root URL to the service instance",
-        "directory": "The directory in which to store the .n3-files",
-        "clean_tmp": "Set iff all files in $AGENT_TMP shall be deleted first",
-    }
-)
-def simulate_model(ctx, origin, directory, clean_tmp=False):
+def run_example(ctx, template_dir, input, selector, origin, directory, clean_tmp=False):
     """Collect definition of specific API composition problem; then solve it."""
 
     # Environment to be used when rendering templates using Jinja2
     ENV = Environment(
-        loader=FileSystemLoader("examples/simulation"),
+        loader=FileSystemLoader(template_dir),
         trim_blocks=True,
         lstrip_blocks=True,
     )
@@ -231,7 +166,6 @@ def simulate_model(ctx, origin, directory, clean_tmp=False):
     g = "00_init_goal.n3"
 
     # Discover _description formulas R_ (RESTdesc descriptions)
-    selector = "ms"
     R = download_restdesc(ctx, origin, selector, directory, False)
 
     # Specify additional _background knowledge B_ [if applicable]
@@ -240,7 +174,7 @@ def simulate_model(ctx, origin, directory, clean_tmp=False):
 
     # Ensure that all relevant knowledge is stored in a file on disk
     for template, filename, data in [
-        (initial, H, {"filepath": os.path.abspath("examples/simulation/model.fmu")}),
+        (initial, H, {"filepath": os.path.abspath(os.path.join(template_dir, input))}),
         (goal, g, {}),
         (background, B, {}),
     ]:
@@ -266,4 +200,16 @@ if __name__ == "__main__":
     directory = os.getenv("AGENT_TMP")
     clean_tmp = True
 
-    simulate_model(Context(), origin, directory, clean_tmp)
+    # Example: Resizing an image
+    templates_dir = "./examples/image_resizing"
+    selector = os.getenv("IMG_API_LANG")
+    input = "example.png"
+
+    run_example(Context(), templates_dir, input, selector, origin, directory, clean_tmp)
+
+    # # Example: Simulation of a Functional Mockup Unit
+    # templates_dir = "./examples/simulation"
+    # selector = "ms"
+    # input = "model.fmu"
+
+    # run_example(Context(), templates_dir, input, selector, origin, directory, clean_tmp)
